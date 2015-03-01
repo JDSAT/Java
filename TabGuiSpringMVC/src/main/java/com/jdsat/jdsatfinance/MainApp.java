ffbb2bf5ff4e27a9223f7e21427e479032d69fe5
garrickra <garrickra@gmail.com>
2015-03-01 10:16:10 -0500

    fixed TabSpringGuiMvc.  Should now work.

--- TabGuiSpringMVC/src/main/java/com/jdsat/jdsatfinance/MainApp.java
+++ TabGuiSpringMVC/src/main/java/com/jdsat/jdsatfinance/MainApp.java
@@ -1,7 +1,5 @@
 package com.jdsat.jdsatfinance;
 
-import com.jdsat.jdsatfinance.data.Database;
-import com.jdsat.jdsatfinance.model.Person;
 import com.jdsat.jdsatfinance.view.MainController;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -14,23 +12,7 @@ public class MainApp extends Application {
     private Stage primaryStage;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        //Setup & test database        
-        
-        Database db=Database.getInstance();
-        Person p=new Person();
-        p.setId(1);
-        p.setName("Andy");
-                
-        System.out.println(db.addPerson(p));
-        System.exit(0);
-       
-        Person p2=db.getPerson(1);
-        System.out.println(p2);
-        System.out.println(p2.getName());
-        System.exit(0);
-        
-        
+    public void start(Stage stage) throws Exception {       
         this.primaryStage=stage;
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppFactory.class);
         MainController mainController = context.getBean(MainController.class);
